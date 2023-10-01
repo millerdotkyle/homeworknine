bf693af1eff4ad8a02f6410da62ae0524844083c
millerdotkyle <kylenotfound@gmail.com>
2023-10-01 18:32:29 -0400

    changed port for listening

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
-const PORT = 3001;
+port = process.env.PORT || 80
 const fs = require('fs')
 
 const app = express();
